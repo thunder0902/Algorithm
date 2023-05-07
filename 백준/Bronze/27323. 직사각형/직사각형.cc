c26316cc0c9f64c5e0b9c7456f8b8dd0713a0d41
thunder0902 <76557909+thunder0902@users.noreply.github.com>
2023-05-07 16:26:04 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+//코딩 원칙 1. 주석을 잘 달아놓자!
+main(){
+
+int a,b;
+scanf("%d\n",&a);
+scanf("%d",&b);
+printf("%d",a*b);
+
+}
